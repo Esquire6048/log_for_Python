7d204f5e5dc969b046c2a1e0e54c6937e76cfc28
Bolero Xu <22206837+Esquire6048@users.noreply.github.com>
2024-05-20 10:38:24 +0000

    Create 665.py

---
+++ 665.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        # Solution 1
+        def is_sorted(nums: List[int]) -> bool:
+            return all(a <= b for a, b in pairwise(nums))
+
+        length = len(nums)
+        for i in range(length - 1):
+            if nums[i] > nums[i + 1]:
+                temp = nums[i]
+                nums[i] = nums[i + 1]
+                if is_sorted(nums):
+                    return True
+                nums[i] = nums[i + 1] = temp
+                return is_sorted(nums)
+        return True
+
+        # Solution 2
+        count = 0
+
+        for i in range(1, len(nums)):
+            if nums[i - 1] > nums[i]:
+                if count == 1:
+                    return False
+                if i - 2 >= 0 and nums[i - 2] > nums[i]:
+                    nums[i] = nums[i - 1]
+                count += 1
+        return True
